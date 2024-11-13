6a069157206ec5ab3f31956a220c5898e50a17ef
ltxstudio <ltxstudio.info@gmail.com>
2024-11-14 05:09:34 +0600

    Create minifier.js

---
+++ src/utils/minifier.js
@@ -0,0 +1,11 @@
+import Terser from 'terser';
+
+export const minifyCode = async (code) => {
+  try {
+    const minified = await Terser.minify(code);
+    return minified.code;
+  } catch (error) {
+    console.error('Minification failed:', error);
+    return null;
+  }
+};
